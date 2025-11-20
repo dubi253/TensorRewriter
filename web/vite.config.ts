04a96c922487b47be9356ef176ba095508160954
Dubi253 <dubi253@outlook.com>
2025-11-20 04:50:04 -0500

    fix: streamline GitHub Actions workflow and update Vite base path

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -22,7 +22,7 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
     plugins: [qwikCity(), qwikVite(), tsconfigPaths({ root: "." })],
-    base: "/TensorRewriter/",
+    base: "/",
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
       // Put problematic deps that break bundling here, mostly those with binaries.
